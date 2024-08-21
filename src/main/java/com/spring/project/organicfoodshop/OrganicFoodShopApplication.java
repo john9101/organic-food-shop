93ee18894122c27df769100fb17025c8dfecce26
john9101 <21130319@st.hcmuaf.edu.vn>
2024-08-21 12:33:09 +0700

    prepare project

---
+++ src/main/java/com/spring/project/organicfoodshop/OrganicFoodShopApplication.java
@@ -0,0 +1,13 @@
+package com.spring.project.organicfoodshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicFoodShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganicFoodShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/project/organicfoodshop/OrganicFoodShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.project.organicfoodshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganicFoodShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
